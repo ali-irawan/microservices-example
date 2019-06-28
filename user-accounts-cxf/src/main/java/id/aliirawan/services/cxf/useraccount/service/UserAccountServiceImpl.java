addddb9b2eb3dbd30c687b14447036258d89908d
Ali Irawan <boylevantz@gmail.com>
2019-06-29 00:11:53 +0700

    add user account service impl

---
+++ user-accounts-cxf/src/main/java/id/aliirawan/services/cxf/useraccount/service/UserAccountServiceImpl.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package id.aliirawan.services.cxf.useraccount.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import id.aliirawan.services.cxf.api.UserAccountService;
+import id.aliirawan.services.cxf.api.model.UserAccount;
+
+/**
+ * @author aliirawan
+ *
+ */
+public class UserAccountServiceImpl implements UserAccountService {
+
+	private List<UserAccount> accounts;
+	
+	public UserAccountServiceImpl() {
+		// Create dummy data
+		accounts = new ArrayList<UserAccount>(); 
+		accounts.add(new UserAccount(100, UserAccount.ACCOUNT_TYPE_GMAIL, "myemail@gmail.com"));
+		accounts.add(new UserAccount(200, UserAccount.ACCOUNT_TYPE_INSTAGRAM, "myinstagram"));
+		accounts.add(new UserAccount(300, UserAccount.ACCOUNT_TYPE_FACEBOOK, "myfacebook"));
+	}
+	
+	@Override
+	public List<UserAccount> findAllByUserId(Integer customerId) {
+		return accounts.stream().filter(a -> a.getId().equals(customerId)).collect(Collectors.toList());
+	}
+
+}
